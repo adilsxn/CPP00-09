07e8d8e05220880567e229bc22ff8bba52c30cb8
adilsxn <24adilsonnunes@gmail.com>
2024-04-27 23:03:28 +0100

    ex05 -- cpp01

---
+++ CPP01/ex05/src/Harl.cpp
@@ -0,0 +1,54 @@
+#include "../inc/Harl.hpp"
+#include <iostream>
+
+Harl::Harl(void){
+    return ;
+};
+
+Harl::~Harl(void){
+    return ;
+}
+
+void Harl::_debug(void){
+    std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+}
+
+void Harl::_info(void){
+    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void Harl::_warning(void){
+    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working since last month.\n";
+}
+
+void Harl::_error(void){
+    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level){
+
+    int  i = -1;
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    while(++i < 4)
+    {
+        if (level == levels[i])
+            break;
+    }
+    switch(i)
+    {
+        case 0:
+            this->_debug();
+            break;
+        case 1:
+            this->_info();
+            break;
+        case 2:
+            this->_warning();
+            break;
+        case 3:
+            this->_error();
+            break;
+        default:
+            return ;
+    }
+}
---
+++ CPP01/ex05/src/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acuva-nu <acuva-nu@student.42lisboa.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 12:34:02 by vgoncalv          #+#    #+#             */
+/*   Updated: 2023/08/06 20:29:18 by acuva-nu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Harl.hpp"
+
+int	main(void)
+{
+    Harl harl;
+
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("DEBUG");
+	return (0);
+}
