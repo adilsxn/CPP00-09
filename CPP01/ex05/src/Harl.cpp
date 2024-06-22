a1b9d0901160bb403a608943d4500823638fb699
acuva-nu <acuva-nu@student.42lisboa.com>
2024-06-22 14:50:06 +0100

    fixing some typos and misunderstandings

--- CPP01/ex05/src/Harl.cpp
+++ CPP01/ex05/src/Harl.cpp
@@ -9,46 +9,46 @@ Harl::~Harl(void){
     return ;
 }
 
+
+
 void Harl::_debug(void){
-    std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    std::cout << "[DEBUG]\nI love having extra bacon for my"
+        <<" 7XL-double-cheese-triple-pickle-special-ketchup"
+        <<" burger. I really do!\n";
 }
 
 void Harl::_info(void){
-    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more "
+        <<"money. You didn't put enough bacon in my burger! If you did, "
+        <<"I wouldn't be asking for more!\n";
 }
 
 void Harl::_warning(void){
-    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working since last month.\n";
+    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for "
+        <<"free. I've been coming for years whereas you started working since" 
+        <<" last month.\n";
 }
 
 void Harl::_error(void){
-    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
+    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the "
+        <<"manager now.\n";
 }
 
 void Harl::complain(std::string level){
 
     int  i = -1;
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*fn[4])(void) = {&Harl::_debug, &Harl::_info, 
+                                &Harl::_warning,&Harl::_error};
+
     while(++i < 4)
     {
         if (level == levels[i])
-            break;
-    }
-    switch(i)
-    {
-        case 0:
-            this->_debug();
-            break;
-        case 1:
-            this->_info();
-            break;
-        case 2:
-            this->_warning();
-            break;
-        case 3:
-            this->_error();
-            break;
-        default:
+        {
+            (this->*fn[i])();
             return ;
+        }
     }
+    std::cout<<"In time you will that nothing really matters\n";
+    return ;
 }
--- CPP01/ex06/src/Harl.cpp
+++ CPP01/ex06/src/Harl.cpp
@@ -10,19 +10,26 @@ Harl::~Harl(void){
 }
 
 void Harl::_debug(void){
-    std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    std::cout << "[DEBUG]\nI love having extra bacon for my"
+        <<" 7XL-double-cheese-triple-pickle-special-ketchup"
+        <<" burger. I really do!\n";
 }
 
 void Harl::_info(void){
-    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+    std::cout << "[INFO]\nI cannot believe adding extra bacon costs more "
+        <<"money. You didn't put enough bacon in my burger! If you did, "
+        <<"I wouldn't be asking for more!\n";
 }
 
 void Harl::_warning(void){
-    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working since last month.\n";
+    std::cout << "[WARNING]\nI think I deserve to have some extra bacon for "
+        <<"free. I've been coming for years whereas you started working since" 
+        <<" last month.\n";
 }
 
 void Harl::_error(void){
-    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
+    std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the "
+        <<"manager now.\n";
 }
 
 void Harl::complain(std::string input){
@@ -52,6 +59,6 @@ void Harl::complain(std::string input){
             this->_error();
             break;
         default:
-            return ;
+            std::cout <<"Yeah, nothing really matters\n";
     }
 }
