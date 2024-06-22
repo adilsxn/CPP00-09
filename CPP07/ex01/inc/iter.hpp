9f383434106ba9fc825e789924d8531e9a0e1e99
Adilson Nunes <66755632+adilsxn@users.noreply.github.com>
2024-06-22 15:55:54 +0100

    Update iter.hpp at cpp07-ex01

--- CPP07/ex01/inc/iter.hpp
+++ CPP07/ex01/inc/iter.hpp
@@ -1,1 +1,26 @@
-adding header file
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstdlib>
+#include <iostream>
+
+template<typename T, typename F>
+void iter(T& arr, size_t size, F function){
+     for(int i = 0; i < sizs; i++)
+            function(arr[i]);
+      return ;
+};
+
+template<typaname T>
+void multbytwo(T& elem){
+     elem  *= 2;
+     returb ;
+};
+
+template<typename T>
+void printArray(T& arr, size_t size){
+   for(int i = 0; i < size; i++)
+       std::cout << "["<<arr[i]<<"]";
+    std::cout << std::endl;
+};
+#endif
