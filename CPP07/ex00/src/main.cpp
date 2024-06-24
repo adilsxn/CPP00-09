32884925cd254e372359693e39461e7760377faa
adilsxn <24adilsonnunes@gmail.com>
2024-06-24 14:09:14 +0100

    adding more tests cpp07, ex00

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -16,34 +16,48 @@
 
 int	main(void)
 {
+    // {
+    //     std::cout <<"-------Test for Int-------------\n";
+    //     int a = 10, b = 5;
+    //     std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
+    //     swap(a, b);
+    //     std::cout <<"After the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
+    //     std::cout <<"The bigger int: \n\t"<<::max(a, b);
+    //     std::cout <<"\nThe smaller int: \n\t"<<::min(a, b);
+    // }
+    // {
+    //     std::cout <<"\n---------Test for Float--------\n";
+    //     float a = 10.3973f, b = 5.3309f;
+    //     std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
+    //     swap(a, b);
+    //     std::cout <<"After the swap: "<<"\na: "<<std::fixed<<a
+    //         <<"\nb: "<<std::fixed<<b<<std::endl;
+    //     std::cout <<"The bigger float: \n\t"<<std::fixed<<::max(a, b);
+    //     std::cout <<"\nThe smaller float: \n\t"<<std::fixed<<::min(a, b);
+    // }
+    // {
+    //     std::cout <<"\n----------Test for String------------\n";
+    //     std::string a = "adilson", b = "adilso";
+    //     std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
+    //     swap(a, b);
+    //     std::cout <<"After the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
+    //     std::cout <<"The bigger string: \n\t"<<::max(a, b);
+    //     std::cout <<"\nThe smaller string: \n\t"<<::min(a, b);
+    //     std::cout<<std::endl;
+    // }
     {
-        std::cout <<"-------Test for Int-------------\n";
-        int a = 10, b = 5;
-        std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
-        swap(a, b);
-        std::cout <<"After the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
-        std::cout <<"The bigger int: \n\t"<<::max(a, b);
-        std::cout <<"\nThe smaller int: \n\t"<<::min(a, b);
-    }
-    {
-        std::cout <<"\n---------Test for Float--------\n";
-        float a = 10.3973f, b = 5.3309f;
-        std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
-        swap(a, b);
-        std::cout <<"After the swap: "<<"\na: "<<std::fixed<<a
-            <<"\nb: "<<std::fixed<<b<<std::endl;
-        std::cout <<"The bigger float: \n\t"<<std::fixed<<::max(a, b);
-        std::cout <<"\nThe smaller float: \n\t"<<std::fixed<<::min(a, b);
-    }
-    {
-        std::cout <<"\n----------Test for String------------\n";
-        std::string a = "adilson", b = "adilso";
-        std::cout <<"Before the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
-        swap(a, b);
-        std::cout <<"After the swap: "<<"\na: "<<a<<"\nb: "<<b<<std::endl;
-        std::cout <<"The bigger string: \n\t"<<::max(a, b);
-        std::cout <<"\nThe smaller string: \n\t"<<::min(a, b);
-        std::cout<<std::endl;
+        int a = 2;
+        int b = 3;
+        ::swap( a, b );
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+        std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+        std::string c = "chaine1";
+        std::string d = "chaine2";
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+        std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     }
     
 	return (0);
