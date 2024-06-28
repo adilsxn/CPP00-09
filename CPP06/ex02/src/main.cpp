c975fc6711416616d910f9b81690aee7cb3e8c4e
adilsxn <24adilsonnunes@gmail.com>
2024-06-28 16:14:20 +0100

    fixing randomness in CPP06-ex02

--- CPP06/ex02/src/main.cpp
+++ CPP06/ex02/src/main.cpp
@@ -9,6 +9,7 @@
 /*   Updated: 2023/08/06 20:29:18 by acuva-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../inc/Main.hpp"
 #include <cstdlib>
 #include <exception>
@@ -19,15 +20,21 @@ Base::~Base(void){
 }
 
 Base* generate(void){
+
     int dice = rand() % 3;
-    Base* ret;
-    if (dice == 0)
-        ret = new A();
-    if (dice == 1)
-        ret = new B();
-    if(dice == 2) 
-        ret = new C();
-    return ret;
+    switch(dice){
+    case 0:
+       std::cout<<"Generating: A\n";
+       return new A();
+    case 1:
+       std::cout<<"Generating: B\n";
+       return new B();
+    case 2:
+       std::cout<<"Generating: C\n";
+       return new C();
+    }
+    std::cout <<"Error happened: cannot generate a class\n";
+    return NULL;
 }
 
 void identify(Base* p){
@@ -47,38 +54,40 @@ void identify(Base& p){
         std::cout <<"Ref. Type A\n";
     }
     catch(std::exception& e){
-        std::cout<<"Invalid Ref. Type A: "<<e.what()<<std::endl;
+        std::cout<<"Invalid Ref. Type A: "<<std::endl;
     }
     try {
         B b  = dynamic_cast<B&>(p);
         std::cout <<"Ref. Type B\n";
     }
     catch(std::exception& e){
-        std::cout<<"Invalid Ref. Type B: "<<e.what()<<std::endl;
+        std::cout<<"Invalid Ref. Type B: "<<std::endl;
     }
     try {
         C c = dynamic_cast<C&>(p);
         std::cout <<"Ref. Type C\n";
     }
     catch(std::exception& e){
-        std::cout<<"Invalid Ref. Type C: "<<e.what()<<std::endl;
+        std::cout<<"Invalid Ref. Type C: "<<std::endl;
     }
 }
 
 int	main(void)
 {
+    std::srand(time(NULL));
     std::cout <<"-----------Correct type-------\n";
     Base* p = generate();
     Base& r = *p; 
+
     identify(p);
     identify(r);
     delete p;
 
     std::cout <<"---------Incorrect type-------\n";
     Base* p2 = new Base();
-    Base& r2 = *p2; 
     identify(p2);
-    identify(r2);
+    identify(*p2);
+
     delete p2;
     return 0;
 }
